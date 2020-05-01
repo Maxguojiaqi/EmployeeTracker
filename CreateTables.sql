be33351e3e79358b6da5600caae1ac19002e0b79
Max <maxguojiaqi@gmail.com>
2020-05-01 14:48:52 -0400

    add logic to handle query and logic

--- CreateTables.sql
+++ CreateTables.sql
@@ -5,7 +5,6 @@ USE companyDB;
 CREATE TABLE department(
   id INTEGER AUTO_INCREMENT NOT NULL,
   name VARCHAR(30),
-  employee
   PRIMARY KEY (id)
 );
 
@@ -16,6 +15,7 @@ CREATE TABLE role(
   department_id INTEGER NOT NULL,
   
   PRIMARY KEY (id)
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
@@ -28,10 +28,49 @@ CREATE TABLE employee(
   PRIMARY KEY (id)
 );
 
-SELECT * FROM department;
-SELECT * FROM role;
-SELECT * FROM employee;
+-- SELECT * FROM department;
+-- SELECT * FROM role;
+-- SELECT * FROM employee;
 
-USE companyDB;
-ALTER TABLE role;
-ALTER COLUMN department_id INTEGER AUTO_INCREMENT NOT NULL;
+-- USE companyDB;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Mike', 'Chan', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Ashley', 'Rodriguez', 3, 0);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Kevin', 'Tupik', 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Malia', 'Brown', 5, 0);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Sarah', 'Lourd', 6, 0);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Tom', 'Allen', 7, 6);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Christian', 'Eckenrode', 3, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Lead', 80000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Person', 50000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Lead Engineer', 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Software Engineer', 80000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Accountant', 60000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Marketing Lead', 100000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Growth Hacker', 90000, 4);
+
+INSERT INTO department (name)
+VALUES ('Sales');
+INSERT INTO department (name)
+VALUES ('Engineering');
+INSERT INTO department (name)
+VALUES ('Finance');
+INSERT INTO department (name)
+VALUES ('Marketing');
--- database.js
+++ database.js
@@ -3,10 +3,13 @@ const inquirer = require("inquirer");
 const cTable = require('console.table');
 
 let connnection
+let allEmployeeInfo
+
 main()
 async function main () {
   try {
     await connect()
+    allEmployeeInfo = await findAllEmployee()
     await createRecord()
   } catch (err) {
     console.error(err)
@@ -35,23 +38,15 @@ async function connect () {
  * D - DELETE - DELETE FROM pets WHERE type = "mouse";
  * 
 Build a command-line application that at a minimum allows the user to:
-
 Add departments, roles, employees
-
 View departments, roles, employees
-
 Update employee roles
 
 Bonus points if you're able to:
-
 Update employee managers
-
 View employees by manager
-
 Delete departments, roles, and employees
-
 View the total utilized budget of a department -- ie the combined salaries of all employees in that department
-
 
  */
 
@@ -59,68 +54,196 @@ View the total utilized budget of a department -- ie the combined salaries of al
  
 async function createRecord () {
 
-    let questionObj = await inquirer.prompt([{
-        type: 'list', 
-        message: `What would you like to do?`,
-        name: 'tableName',
-        choices: ['View All Employee','View All Employee by Role','View All Employee by Department','Add Employee','Remove Employee','Update Employee Role', 'Update Employee Manager']
-        }])
 
-
+    let keepLoop = true
     // depending on the table, ask more questions
+    while(keepLoop)
+    {
+    let questionObj = await inquirer.prompt([{
+      type: 'list', 
+      message: `What would you like to do?`,
+      name: 'tableName',
+      choices: ['View All Employee','View All Employee by Role','View All Employee by Department','Add Employee','Remove Employee','Update Employee Role', 'Update Employee Manager']
+      }])
+
     switch(questionObj.tableName){
         case 'View All Employee': 
-          let recordsVAE = await findAllEmployee()
-          console.table(records)
+          // let recordsVAE = await findAllEmployee()
+          console.table(allEmployeeInfo)
           break;
 
         case 'View All Employee by Role': 
-          let selectionOptions = await findAllRoleType();
-          console.log(selectionOptions)
-          let questionObj = await inquirer.prompt([{
+          let selectionOptionsRolewithID = await findAllRoleType();
+          let selectionOptionsRole = selectionOptionsRolewithID.map(row => row.title)
+          let questionVAER = await inquirer.prompt([{
             type: 'list', 
             message: `What would you like to do?`,
             name: 'roleType',
-            choices: selectionOptions
+            choices: selectionOptionsRole
             }])
-          console.log(questionObj.roleType)
-          let recordsVAER = await findAllEmployeeByRole(questionObj.roleType)
+          let recordsVAER = await findAllEmployeeByRole(questionVAER.roleType)
           console.table(recordsVAER)
           break;
-            // let employeeQuestion = await inquirer.prompt([{
-            //     message: `Please enter the new employee first name`,
-            //     name: 'employeeFirstname'
-            //     },
-            //     {
-            //     message: `Please enter the new employee last name`,
-            //     name: 'employeeLastname'
-            //     },
-            //     {
-            //     message: `Please enter the new employee's role`,
-            //     name: 'employeeTitle'
-            //     },
-            //     {
-            //         message: `Please enter the new employee's Manager`,
-            //         name: 'employeeManager'
-            //     }])
             
         case 'View All Employee by Department':
-            let roleQuestion = await inquirer.prompt([{
-                message: `Please enter the new role's title`,
-                name: 'roleTitle'
-                },
-                {
-                message: `Please enter the new role's salary`,
-                name: 'roleSalary'
-                }])
+          let selectionOptionsDepartment = await findAllDepartmentType();
+          let questionVAED = await inquirer.prompt([{
+            type: 'list', 
+            message: `What would you like to do?`,
+            name: 'departmentType',
+            choices: selectionOptionsDepartment
+            }])
+          let recordsVAED = await findAllEmployeeByDepartment(questionVAED.departmentType)
+          console.table(recordsVAED)
+          break;
+          
+        case 'Add Employee':
+          let allRolesWithID = await findAllRoleType();
+          let allRoles = allRolesWithID.map(row => row.title)
+          let managerNameswithID = await findAllManagerAndID();
+          let managerNames = managerNameswithID.map(row => row.managerName);
+          // If no manager applicable, user will pick NONE, add this option to the selections
+          managerNames.push('NONE')
+          let questionAE = await inquirer.prompt([{
+            message: `Please enter the new employee first name:`,
+            name: 'employeeFirstname'
+            },
+            {
+            message: `Please enter the new employee last name:`,
+            name: 'employeeLastname'
+            },
+            {
+              type: 'list', 
+              message: `Please select the new employee's role:`,
+              name: 'roleType',
+              choices: allRoles
+            },
+            {
+              type: 'list', 
+              message: `Please select the new employee's manager:`,
+              name: 'managerName',
+              choices: managerNames
+            }])
 
-            recordObj = {
-                title: roleQuestion.title,
-                salary: roleQuestion.salary
-            }
-            break;
-        }
+          console.log('Manager Name:' + questionAE.managerName);
+          console.log('Role Type:' + questionAE.roleType);
+          console.log(managerNameswithID);
+          console.log(allRolesWithID);
+          let managerID = 0
+          managerNameswithID.forEach(record => {
+            if (record.managerName == questionAE.managerName) managerID = record.id
+          });
+          let roleID = 0
+          allRolesWithID.forEach(record => {
+            if (record.title == questionAE.roleType) roleID = record.id
+          });
+          console.log('Manager ID:' + managerID);
+          console.log('Role ID:' + roleID);
+          let newEmployeeObj = {
+            first_name: questionAE.employeeFirstname,
+            last_name: questionAE.employeeLastname,
+            role_id: roleID,
+            manager_id: managerID
+          }
+          await addNewEmployee(newEmployeeObj)
+          break;
 
+          case 'Remove Employee':
+            let allEmployeeNamesRE = []
+            let allEmployeeNameswithIDRE = []
+            allEmployeeInfo.forEach(element => {
+              let fullName = element.FirstName +' ' + element.LastName
+              allEmployeeNamesRE.push(fullName)
+              allEmployeeNameswithIDRE.push({name: fullName, id: element.ID})
+            });
+            console.log(allEmployeeNamesRE)
+
+            let questionRE = await inquirer.prompt([{
+              type: 'list', 
+              message: `Which Employee Info you'd like to Remove:`,
+              name: 'employeeName',
+              choices: allEmployeeNamesRE
+              }])
+
+            let employeeObj = allEmployeeNameswithIDRE.find((element)=>element.name == questionRE.employeeName)
+            await deleteEmployee(employeeObj.id)
+
+          case 'Update Employee Role':
+            let allEmployeeNamesUER = []
+            let allEmployeeNameswithIDUER = []
+            allEmployeeInfo.forEach(element => {
+              let fullName = element.FirstName +' ' + element.LastName
+              allEmployeeNamesUER.push(fullName)
+              allEmployeeNameswithIDUER.push({name: fullName, id: element.ID})
+            });
+            console.log(allEmployeeNamesUER)
+            let allRolesWithIDUER = await findAllRoleType();
+            let allRolesUER = allRolesWithIDUER.map(row => row.title)
+
+            let questionUER = await inquirer.prompt([{
+              type: 'list', 
+              message: `Which Employee Info you'd like to Update:`,
+              name: 'employeeName',
+              choices: allEmployeeNamesUER
+              },
+              {
+                type: 'list', 
+                message: `Which Role you'd like to assign to this Employee:`,
+                name: 'roleName',
+                choices: allRolesUER
+              }])
+
+            let employeeObjUER = allEmployeeNameswithIDUER.find((element)=>element.name == questionUER.employeeName)
+            console.log(allRolesWithIDUER)
+            let roleObjUER = allRolesWithIDUER.find((element)=>element.title == questionUER.roleName)
+            await updateEmployeeRole(roleObjUER.id, employeeObjUER.id)
+
+          case 'Update Employee Manager':
+            let allEmployeeNamesUEM = []
+            let allEmployeeNameswithIDUEM = []
+            allEmployeeInfo.forEach(element => {
+              let fullName = element.FirstName +' ' + element.LastName
+              allEmployeeNamesUEM.push(fullName)
+              allEmployeeNameswithIDUEM.push({name: fullName, id: element.ID})
+            });
+            console.log(allEmployeeNamesUEM)
+
+            let managerNameswithIDUEM = await findAllManagerAndID();
+            let managerNamesUEM = managerNameswithIDUEM.map(row => row.managerName);
+
+            let questionUEM1 = await inquirer.prompt([{
+              type: 'list', 
+              message: `Which employee info you'd like to update:`,
+              name: 'employeeName',
+              choices: allEmployeeNamesUEM
+              }])
+            // if the person exist from the manager's list, remove him; so they won't report to themselvs
+            managerNamesUEM.forEach((element,index) => {
+              if(element === questionUEM1.employeeName) managerNamesUEM.splice(index,1);
+            });
+            // add option to not set their manager
+            managerNamesUEM.push('NONE')
+
+            let questionUEM2 = await inquirer.prompt([{
+                type: 'list', 
+                message: `Which manager you'd like this Employee to report to:`,
+                name: 'managerName',
+                choices: managerNamesUEM
+              }])
+
+            let employeeObjUEM = allEmployeeNameswithIDUEM.find((element)=>element.name == questionUEM1.employeeName)
+            let managerObjUEM = managerNameswithIDUEM.find((element)=>element.managerName == questionUEM2.managerName)
+            let managerIDUEM = 0
+            // if there is a manager assigned, update the managerID
+            if (managerObjUEM) managerIDUEM = managerObjUEM.id
+
+            console.log(managerNameswithIDUEM)
+            console.log(employeeObjUEM)
+            console.log(managerObjUEM)
+            await updateEmployeeManager(managerIDUEM, employeeObjUEM.id)
+
+          }
+    }
   }
 
 
@@ -130,53 +253,52 @@ async function findAllEmployee(){
 }
 
 async function findAllRoleType(){
-  const [rows] = await connection.query('SELECT title FROM role')
-  return rows.map(row => row.title)
-}
-
-async function findAllEmployeeByRole(roleType){
-  console.log(roleType)
-  const [rows] = await connection.query(`SELECT e.id as ID, e.first_name as FirstName, e.last_name as LastName, r.title as Title, r.salary as Salary, d.name as Department, CONCAT(eManager.last_name," ",eManager.first_name) AS ManagerName FROM employee e LEFT JOIN role r on e.role_id = r.id LEFT JOIN department d on d.id = r.department_id LEFT JOIN employee eManager on eManager.id = e.manager_id WHERE Title = "${roleType}"`)
+  const [rows] = await connection.query('SELECT title, id FROM role')
   return rows
 }
 
-async function findSongsByArtist () {
+async function findAllManagerAndID(){
+  const [rows] = await connection.query('SELECT CONCAT(first_name," ",last_name) as managerName, id FROM employee WHERE manager_id > 0')
+  return rows
+}
 
-    let questionObj = await inquirer.prompt([{
-        message: `Which Artist you would like to find?`,
-        name: 'artistName'
-        }])
+async function findAllEmployeeByRole(roleType){
+  const [rows] = await connection.query(`SELECT e.id as ID, e.first_name as FirstName, e.last_name as LastName, r.title as Title, r.salary as Salary, d.name as Department, CONCAT(eManager.last_name," ",eManager.first_name) AS ManagerName FROM employee e LEFT JOIN role r on e.role_id = r.id LEFT JOIN department d on d.id = r.department_id LEFT JOIN employee eManager on eManager.id = e.manager_id WHERE r.title = "${roleType}"`)
+  return rows
+}
 
-    const [rows] = await connection.query('SELECT * FROM top5000 WHERE ?', {
-      artist: questionObj.artistName
-    })
-    console.log(rows, '\n')
-  }
+async function findAllDepartmentType(){
+  const [rows] = await connection.query('SELECT name FROM department')
+  return rows.map(row => row.name)
+}
 
-async function findArtistsMoreThanOnce () {
-    const [rows] = await connection.query('SELECT artist FROM top5000 GROUP BY artist HAVING COUNT(artist) > 1;')
-    console.log(rows, '\n')
-  }
+async function findAllEmployeeByDepartment(departmentType){
+  const [rows] = await connection.query(`SELECT e.id as ID, e.first_name as FirstName, e.last_name as LastName, r.title as Title, r.salary as Salary, d.name as Department, CONCAT(eManager.last_name," ",eManager.first_name) AS ManagerName FROM employee e LEFT JOIN role r on e.role_id = r.id LEFT JOIN department d on d.id = r.department_id LEFT JOIN employee eManager on eManager.id = e.manager_id WHERE d.name = "${departmentType}"`)
+  return rows
+}
 
-async function findRawTotalGreaterThan () {
 
-    let questionObj = await inquirer.prompt([{
-        message: `Find artist with a raw record number of ..?`,
-        name: 'rawRecord'
-        }])
+async function addNewEmployee (newEmployeeObj) {
 
-    const [rows] = await connection.query(`SELECT * FROM top5000 WHERE raw_total > ${questionObj.rawRecord}`)
-    // select position.song.artist.year FROM top5000 WHERE position BETWEEN 45 AND 55;
-    console.log(rows, '\n')
-  }
+  const [results] = await connection.query('INSERT INTO employee SET ?', newEmployeeObj)
+  console.log(results.affectedRows + ' employee inserted!\n')
+}
 
-  async function findSoung () {
+async function deleteEmployee(employeeID){
+  const [results] = await connection.query(`DELETE FROM employee WHERE id = ${employeeID}`)
+  console.log(results.affectedRows + ' employee Deleted!\n')
 
-    let questionObj = await inquirer.prompt([{
-        message: `Which Song you would like to find?`,
-        name: 'songName'
-        }])
+}
 
-    const [rows] = await connection.query(`SELECT * FROM top5000 WHERE song LIKE '%${questionObj.songName}%'` )
-      console.log(rows, '\n')
-  }
+async function updateEmployeeRole(roleID, employeeID){
+
+  const [results] = await connection.query(`UPDATE employee SET role_id = ${roleID} WHERE id=${employeeID}`)
+  console.log(results.affectedRows + ' employee Updated!\n')
+}
+
+async function updateEmployeeManager(managerID, employeeID){
+
+  if (managerID == undefined) managerID = 0
+  const [results] = await connection.query(`UPDATE employee SET manager_id = ${managerID} WHERE id=${employeeID}`)
+  console.log(results.affectedRows + ' employee Updated!\n')
+}
